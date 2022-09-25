15062906abde89cc97ce8027de0aede592ccd78f
Serhii Kuhaiev <kugaew@gmail.com>
2022-09-26 00:19:51 +0200

    Making exercise-1 paragraph 1, 2. Removing redirect on <a> click

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  divGalleryEl: document.querySelector(".gallery"),
+};
+
+const insertGalleryEl = galleryItems.map((galleryItem) => {
+  const galleryItemEl = document.createElement("div");
+  const galleryLinkEl = document.createElement("a");
+  const galleryImageEl = document.createElement("img");
+
+  galleryImageEl.classList.add("gallery__image");
+  galleryImageEl.setAttribute("src", galleryItem.preview);
+  galleryImageEl.setAttribute("alt", galleryItem.description);
+  galleryImageEl.dataset.source = galleryItem.original;
+
+  galleryLinkEl.classList.add("gallery__link");
+  galleryLinkEl.setAttribute("href", galleryItem.original);
+
+  galleryItemEl.classList.add("gallery__item");
+
+  galleryLinkEl.append(galleryImageEl);
+  galleryItemEl.append(galleryLinkEl);
+
+  return galleryItemEl;
+});
+
+refs.divGalleryEl.append(...insertGalleryEl);
+
+refs.divGalleryEl.addEventListener("click", onOpenModalImage);
+
+function onOpenModalImage(event) {
+  event.preventDefault();
+  console.log(event);
+  console.log("original image: ", event.target.dataset.source);
+}
