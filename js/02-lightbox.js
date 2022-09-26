1b06b9113cbd06552bbaa2e09183ab3cbcc9634e
Serhii Kuhaiev <kugaew@gmail.com>
2022-09-26 13:27:27 +0200

    Making gallery of exercise-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  divGalleryEl: document.querySelector(".gallery"),
+};
+
+refs.divGalleryEl.append(...createGallerySrtucture(galleryItems));
+
+function createGallerySrtucture(arrOfGallaryItems) {
+  return arrOfGallaryItems.map((galleryItem) => {
+    const galleryLinkEl = document.createElement("a");
+    const galleryImageEl = document.createElement("img");
+
+    galleryImageEl.classList.add("gallery__image");
+    galleryImageEl.setAttribute("src", galleryItem.preview);
+    galleryImageEl.setAttribute("alt", galleryItem.description);
+
+    galleryLinkEl.classList.add("gallery__item");
+    galleryLinkEl.setAttribute("href", galleryItem.original);
+
+    galleryLinkEl.append(galleryImageEl);
+
+    return galleryLinkEl;
+  });
+}
